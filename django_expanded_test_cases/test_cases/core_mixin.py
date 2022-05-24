0b4b9f2f3b63913d0c508fc75f54f6977c8be7ca
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-05-24 10:38:47 -0400

    Add basic get_user function

--- django_expanded_test_cases/test_cases/core_mixin.py
+++ django_expanded_test_cases/test_cases/core_mixin.py
@@ -6,8 +6,73 @@ Core testing logic, universal to all test cases.
 from django.contrib.auth import get_user_model
 
 
-class CoreTestCaseMixin:
-    """Core testing logic, used in all other expanded TestCase classes."""
 
-    def setUp(self):
-        pass
+class CoreTestCaseMixin:
+    """Core testing logic, used in all other expanded TestCase classes.
+
+    For compatibility, does not inherit from
+        django.test.TestCase
+        channels.testing.ChannelsLiveServerTestCase
+
+    Inheriting from either (seems to) make it incompatible with the other.
+    Therefore we treat this as a separate mixin that inherits from nothing, and is included in all.
+    """
+
+    def set_up(self):
+        """
+        Acts as the equivalent of the UnitTesting "setUp()" function.
+
+        However, since this is not inheriting from a given TestCase, calling the literal function
+        here would override instead.
+        """
+        # Generate "special case" test user instances.
+        # Guarantees that there will always be at least some default User models when tests are run.
+        self.test_superuser = get_user_model().objects.create_user(
+            username='test_superuser',
+            password='password',
+            is_superuser=True,
+        )
+        self.test_admin = get_user_model().objects.create_user(
+            username='test_admin',
+            password='password',
+            is_staff=True,
+        )
+        self.test_user = get_user_model().objects.create_user(username='test_user', password='password')
+
+    def get_user(self, user, password='password'):
+        """Returns user matching provided value.
+
+        :param user: User model, or corresponding username, to use.
+        :param password: Password str to assign to user.
+        """
+
+        # Check if instance is User model.
+        if isinstance(user, get_user_model()):
+            # Already User model. This is fine.
+            pass
+
+        # Handle all "special cases" for testing logic.
+        elif user == 'test_superuser':
+            user = self.test_superuser
+        elif user == 'test_admin':
+            user = self.test_admin
+        elif user == 'test_user':
+            user = self.test_user
+
+        else:
+            # Is not User model. Get or create.
+            try:
+                user = get_user_model().objects.get(username=str(user))
+            except get_user_model().DoesNotExist:
+                user = get_user_model().objects.create(username=str(user))
+
+        # Handle passwords.
+        password = str(password).strip()
+        if len(password) == 0:
+            # Empty password. Reset back to default.
+            password = 'password'
+        user.set_password(password)
+        user.unhashed_password = password
+        user.save()
+
+        return user
--- tests/test_cases/test_base_case.py
+++ tests/test_cases/test_base_case.py
@@ -3,8 +3,50 @@ Tests for test_cases/base_test_case.py.
 """
 
 # System Imports.
-from django.test import TestCase
+
+# User Imports.
+from django_expanded_test_cases.test_cases import BaseTestCase
 
 
-class BaseClassTest(TestCase):
+class BaseClassTest(BaseTestCase):
     """Tests for BaseTestCase class."""
+
+    def setUp(self):
+        # Run parent setup logic.
+        super().setUp()
+        # Also call CoreMixin setup logic.
+        self.set_up()
+
+    def test__get_user(self):
+        """
+        Tests get_user() function.
+        """
+        with self.subTest('Test "test_superuser" user - Default password'):
+            test_superuser = self.get_user('test_superuser')
+
+            self.assertEqual(test_superuser, self.test_superuser)
+            self.assertEqual(test_superuser.username, 'test_superuser')
+            self.assertTrue(test_superuser.check_password('password'))
+            self.assertEqual(test_superuser.unhashed_password, 'password')
+            self.assertEqual(test_superuser.is_superuser, True)
+            self.assertEqual(test_superuser.is_staff, False)
+
+        with self.subTest('Test "test_admin" user - Default password'):
+            test_admin = self.get_user('test_admin')
+
+            self.assertEqual(test_admin, self.test_admin)
+            self.assertEqual(test_admin.username, 'test_admin')
+            self.assertTrue(test_admin.check_password('password'))
+            self.assertEqual(test_admin.unhashed_password, 'password')
+            self.assertEqual(test_admin.is_superuser, False)
+            self.assertEqual(test_admin.is_staff, True)
+
+        with self.subTest('Test "test_user" user - Default password'):
+            test_user = self.get_user('test_user')
+
+            self.assertEqual(test_user, self.test_user)
+            self.assertEqual(test_user.username, 'test_user')
+            self.assertTrue(test_user.check_password('password'))
+            self.assertEqual(test_user.unhashed_password, 'password')
+            self.assertEqual(test_user.is_superuser, False)
+            self.assertEqual(test_user.is_staff, False)
