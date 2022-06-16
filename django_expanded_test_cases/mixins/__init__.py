cc169eb96818cc8d5592af8a7976993382dafcd0
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-06-16 02:49:35 -0400

    Correct comments in __init__.py file

--- django_expanded_test_cases/mixins/__init__.py
+++ django_expanded_test_cases/mixins/__init__.py
@@ -3,9 +3,9 @@ Imports logic for "django_expanded_test_cases/mixins/" folder.
 Makes project imports to this folder behave like a standard file.
 """
 
-# Expanded "Base" TestCase utility class.
+# Expanded "Base" TestCase utility mixins.
 from .core_mixin import CoreTestCaseMixin
 
 
-# Expanded "Integration" TestCase utility class.
+# Expanded "Integration" TestCase utility mixins.
 from .response_mixin import ResponseTestCaseMixin
