9e40276a04368cbbc53826bbfaf73d458425930e
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-06-21 10:31:26 -0400

    Remove unhelpful extra subprocess output on pytest failure

--- runpytests.py
+++ runpytests.py
@@ -6,12 +6,13 @@ import sys
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def runtests():
     """Run Tests"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
     os.environ.setdefault('PYTHONPATH', ROOT_DIR)
     argv = ['pytest'] + sys.argv[1:] + ['--asyncio-mode=auto']
-    subprocess.run(argv, check=True)
+    subprocess.run(argv, check=False)
 
 
 if __name__ == '__main__':
