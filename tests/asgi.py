5de63fc136c48d36ce92c08bb0c296ec6be108a9
Brandon Rodriguez <brodriguez8774@gmail.com>
2023-07-14 15:41:35 -0400

    Correct depreciation warning for channels import

--- tests/asgi.py
+++ tests/asgi.py
@@ -4,8 +4,15 @@ import os
 
 # Third-Party Imports.
 import django
-from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter
+
+# Import handling for post-Django V2.
+try:
+    from django.core.asgi import get_asgi_application as AsgiHandler
+    # Success. Is Django3 or higher.
+except ImportError:
+    # Failure. Likely Django 2.
+    from channels.http import AsgiHandler
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
